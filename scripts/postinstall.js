6f8eb229ea32a32681b35422d8a7a7f3d72e9459
Ali Muhammad <ali.munchkin08@gmail.com>
2025-05-13 14:19:50 +0500

    building locally

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,19 +1,23 @@
+import { exec, execSync } from "node:child_process";
 import { DEVMODE } from "../config.js";
 
 import { rmSync } from "node:fs";
 
 async function cleanDist() {
-  if(process.platform !== "win32"){
+  if (process.platform !== "win32") {
     rmSync("dist/win", { recursive: true, force: true });
   }
-  if(process.platform !== "darwin"){
-    rmSync("dist/darwin", { recursive: true, force: true });
-  }
-  if(process.platform !== "linux"){
+  if (process.platform !== "linux") {
     rmSync("dist/linux", { recursive: true, force: true });
   }
 }
 
 if (!DEVMODE) {
   cleanDist();
+  if (!process.platform === "win32" || !process.platform === "linux") {
+    console.log(
+      "Prebuilt binaries are not available for this platform. Building the native addon."
+    );
+    execSync("npm run dist");
+  }
 }
